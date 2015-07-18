4ed5215afe1cdc50775d371e1a96f2e9624626b8
Naoki Morita <namorit@gmail.com>
2015-07-19 01:33:56 +0900

    Carthage compatible

---
+++ SwiftFontName/SwiftFontName.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFontName.h
+//  SwiftFontName
+//
+//  Created by MORITANAOKI on 2015/07/19.
+//  Copyright (c) 2015年 molabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFontName.
+FOUNDATION_EXPORT double SwiftFontNameVersionNumber;
+
+//! Project version string for SwiftFontName.
+FOUNDATION_EXPORT const unsigned char SwiftFontNameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFontName/PublicHeader.h>
+
+
